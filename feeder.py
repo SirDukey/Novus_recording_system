bc5776958468176179562af083f999f167b05739
SirDukey <michael_duke@outlook.com>
2018-09-05 09:36:29 +0200

    creating feeder.py

---
+++ feeder.py
@@ -0,0 +1,10 @@
+import subprocess as sp
+from app import content
+
+CMS_DICT = content()
+
+for item in CMS_DICT['tv']:
+
+    feed = sp.Popen(['ffmpeg', '-nostats', '-loglevel', '0', '-xerror',
+                     '-i', item[1], 'http://127.0.0.1:8090/{}.ffm'.format(item[0])])
+    print(item[0], 'feed on PID:', str(feed.pid))
