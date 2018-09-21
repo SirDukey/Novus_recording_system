8416fdf64c960397448320b02e82def2285e0c20
SirDukey <michael_duke@outlook.com>
2018-09-21 11:52:00 +0200

    m3u8 changes

--- radio_recorder.py
+++ radio_recorder.py
@@ -137,7 +137,7 @@ def rad_record_m3u8(name, url):
                             #'-re',
                             #'-nostats',
                             #'-loglevel', '0',
-                            #'-xerror',
+                            '-xerror',
                             '-progress', 'timestamps/' + name + '.ts',
                             #'-y'
                             '-live_start_index', '-1',
