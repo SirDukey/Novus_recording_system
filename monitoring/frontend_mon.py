04dc5da26b3266a0f5c9806f285356b90a910fa8
SirDukey <michael_duke@outlook.com>
2018-10-23 15:45:29 +0200

    adding services monitoring

---
+++ monitoring/frontend_mon.py
@@ -0,0 +1,16 @@
+import subprocess as sp
+
+res = sp.Popen(['systemctl', 'is-active', 'Novus_recording_system.service'], stdout=sp.PIPE, stderr=sp.PIPE)
+output, error = res.communicate()
+output = output.decode('ascii')
+error = error.decode('ascii')
+
+if output:
+    output = output.split('\n')
+    if output[0] == 'active':
+        print(1)
+    elif output[0] == 'inactive':
+        print(0)
+
+elif error:
+    print(3)
